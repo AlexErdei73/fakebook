0dd4372a4b36b9742f37fdd0900931ad05ad75f9
Alex Erdei <mralexerdei@yahoo.co.uk>
2021-04-27 11:55:10 +0100

    add firebase to the react app

--- src/index.js
+++ src/index.js
@@ -1,14 +1,30 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import App from "./App";
+import reportWebVitals from "./reportWebVitals";
+import * as firebase from "firebase";
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+var firebaseConfig = {
+  apiKey: "AIzaSyA6giH2VCl9pBrO86uGH3gcwbmeM-dYMPM",
+  authDomain: "fakebook-2df7b.firebaseapp.com",
+  projectId: "fakebook-2df7b",
+  storageBucket: "fakebook-2df7b.appspot.com",
+  messagingSenderId: "1030600439600",
+  appId: "1:1030600439600:web:98617c1d503260115cfaf5",
+  measurementId: "G-YN35E2SN2E",
+};
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+firebase.analytics();
 
 ReactDOM.render(
   <React.StrictMode>
     <App />
   </React.StrictMode>,
-  document.getElementById('root')
+  document.getElementById("root")
 );
 
 // If you want to start measuring performance in your app, pass a function
