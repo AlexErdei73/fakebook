f3712ef98e27cf28c75911b91ced8cd70f442903
Alex Erdei <mralexerdei@yahoo.co.uk>
2021-05-03 03:29:38 +0100

    add reactfire library to use firebase

--- src/App.js
+++ src/App.js
@@ -1,24 +1,13 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
+import { useFirebaseApp } from "reactfire";
 
 function App() {
+  const firebase = useFirebaseApp();
+  console.log(firebase);
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    //some code
+    <h1>Fakebook</h1>
   );
 }
 
--- src/index.js
+++ src/index.js
@@ -1,33 +1,17 @@
-import React from "react";
+import React, { Suspense } from "react";
 import ReactDOM from "react-dom";
+import { FirebaseAppProvider } from "reactfire";
+import firebaseConfig from "./firebaseConfig";
 import "./index.css";
 import App from "./App";
-import reportWebVitals from "./reportWebVitals";
-import * as firebase from "firebase";
-
-// Your web app's Firebase configuration
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
-var firebaseConfig = {
-  apiKey: "AIzaSyA6giH2VCl9pBrO86uGH3gcwbmeM-dYMPM",
-  authDomain: "fakebook-2df7b.firebaseapp.com",
-  projectId: "fakebook-2df7b",
-  storageBucket: "fakebook-2df7b.appspot.com",
-  messagingSenderId: "1030600439600",
-  appId: "1:1030600439600:web:98617c1d503260115cfaf5",
-  measurementId: "G-YN35E2SN2E",
-};
-// Initialize Firebase
-firebase.initializeApp(firebaseConfig);
-firebase.analytics();
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  <FirebaseAppProvider firebaseConfig={firebaseConfig}>
+    <Suspense fallback={<h3>Loading...</h3>}>
+      <React.StrictMode>
+        <App />
+      </React.StrictMode>
+    </Suspense>
+  </FirebaseAppProvider>,
   document.getElementById("root")
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
