b06dc52dc557f84ee86157ab9a7cad690f715cf4
Alex Erdei <mralexerdei@yahoo.co.uk>
2021-09-01 20:48:48 +0100

    remove inline css from FriendCard

--- src/components/FriendCard.jsx
+++ src/components/FriendCard.jsx
@@ -4,49 +4,38 @@ import CircularImage from "./CircularImage";
 import { StorageImage } from "reactfire";
 
 const FriendCard = (props) => {
+  const { user, key } = props;
+
   const userName = `${props.user.firstname} ${props.user.lastname}`;
 
   const [showOverlay, setShowOverlay] = useState(false);
 
   return (
-    <Col key={props.key} xs={6} className="my-3">
+    <Col key={key} xs={6} className="my-3">
       <OverlayTrigger
         placement="auto"
         show={showOverlay}
         overlay={
           <div
-            style={{
-              position: "relative",
-              width: "400px",
-              height: "200px",
-              background: "white",
-              border: "2px solid lightgray",
-              borderRadius: "10px",
-            }}
+            id="popup-card"
             onMouseEnter={() => setShowOverlay(true)}
             onMouseLeave={() => setShowOverlay(false)}
           >
             <div className="m-3">
-              <CircularImage size="120" url={props.user.profilePictureURL} />
+              <CircularImage size="120" url={user.profilePictureURL} />
             </div>
-            <h4
-              style={{
-                position: "absolute",
-                top: "20px",
-                left: "160px",
-              }}
-            >
+            <h4 id="name-tag">
               <b>{userName}</b>
             </h4>
           </div>
         }
       >
         <StorageImage
-          storagePath={props.user.profilePictureURL}
+          storagePath={user.profilePictureURL}
           width="90px"
           height="90px"
           alt=""
-          style={{ objectFit: "cover" }}
+          id="profile-picture"
           onMouseEnter={() => setShowOverlay(true)}
           onMouseLeave={() => setShowOverlay(false)}
         />
