e5c6552a31b6f08e4d8ca4bd6efb3bac38331ce6
Alex Erdei <mralexerdei@yahoo.co.uk>
2021-10-22 17:32:44 +0100

    add imgFriends to FriendListPage

--- src/components/FriendsListPage.jsx
+++ src/components/FriendsListPage.jsx
@@ -4,6 +4,7 @@ import FriendList from "./FriendList";
 import { Row, Col } from "react-bootstrap";
 import { useLocation } from "react-router-dom";
 import "./FriendsListPage.css";
+import imgFriends from "../images/friends.jpg";
 
 const FriendsListPage = (props) => {
   const { users, user, userID } = props;
@@ -19,7 +20,17 @@ const FriendsListPage = (props) => {
       <FriendList users={users} />
       <Col id="col-2" className="overflow-auto mh-100 hide-scrollbar">
         {isNoUser ? (
-          "Select people's names to preview their profile."
+          <div className="h-100 w-100 d-flex flex-column align-items-center justify-content-center">
+            <img
+              width="200px"
+              src={imgFriends}
+              alt="cartoon of fakebook friends"
+              className="p-4"
+            />
+            <h5 className="text-muted">
+              <b>Select people's names to preview their profile.</b>
+            </h5>
+          </div>
         ) : (
           <div id="profile-container">
             <Profile user={user} userID={userID} users={users} />
