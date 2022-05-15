157053ef11cb607c59865051efcd7ce2887af6f3
Alex Erdei <mralexerdei@yahoo.co.uk>
2022-05-15 14:03:42 +0100

    Fix warning in App.js

--- src/App.js
+++ src/App.js
@@ -6,7 +6,6 @@ import { useSigninCheck } from "reactfire";
 import "bootstrap/dist/css/bootstrap.min.css";
 import Col from "react-bootstrap/Col";
 import Row from "react-bootstrap/Row";
-import Container from "react-bootstrap/Container";
 import RecentLogins from "./components/RecentLogins";
 import Button from "react-bootstrap/Button";
 import Modal from "react-bootstrap/Modal";
