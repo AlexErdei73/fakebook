ecb0753042cfeb6ebdbcdfbb7fc7085c17300919
Alex Erdei <mralexerdei@yahoo.co.uk>
2022-07-16 09:33:43 +0100

    Add observer to listen to auth state changes

--- src/backend/backend.js
+++ src/backend/backend.js
@@ -1,6 +1,9 @@
 import firebase from "firebase/app";
 import "firebase/storage";
+import "firebase/auth";
 import firebaseConfig from "../firebaseConfig";
+import store from "../app/store";
+import { signIn, signOut } from "../features/user/userSlice";
 
 firebase.initializeApp(firebaseConfig);
 const storage = firebase.storage();
@@ -10,3 +13,15 @@ export async function getImageURL(imagePath) {
   const url = await imageRef.getDownloadURL();
   return url;
 }
+
+const auth = firebase.auth();
+
+auth.onAuthStateChanged((user) => {
+  if (user) {
+    const id = user.uid;
+    const isEmailVerified = user.emailVerified;
+    store.dispatch(signIn({ id, isEmailVerified }));
+  } else {
+    store.dispatch(signOut());
+  }
+});
