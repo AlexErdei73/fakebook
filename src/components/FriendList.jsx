37708b8754131463222559d47a6c27f7188fe8f2
Alex Erdei <mralexerdei@yahoo.co.uk>
2022-07-26 09:05:09 +0100

    Fix bug in FriendList

--- src/components/FriendList.jsx
+++ src/components/FriendList.jsx
@@ -5,16 +5,20 @@ import { Link } from "react-router-dom";
 import { useSelector } from "react-redux";
 
 const FriendList = (props) => {
-  const { variant } = props;
+  const { users, variant } = props;
 
-  const users = useSelector((state) => state.users);
+  const allUsers = useSelector((state) => state.users);
+
+  //Component is used with users prop in LikesModal, but without users prop in FriendsListPage
+  let usersToUse = allUsers;
+  if (users) usersToUse = users;
 
   const isModal = variant === "modal";
 
   return (
     <Col xs="auto" className="overflow-auto mh-100">
       <div id="col-1" className="m-2">
-        {users.map((user, index) => {
+        {usersToUse.map((user, index) => {
           let profileLink = `/fakebook/${user.lastname}.${user.firstname}`;
           if (user.index && user.index > 0)
             profileLink = profileLink + `.${user.index}`;
