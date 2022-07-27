2f34a9caaa315f1a3e1c536cbe62d48628008cae
Alex Erdei <mralexerdei@yahoo.co.uk>
2022-07-27 17:38:24 +0100

    Remove reactfire from index.js

--- src/index.js
+++ src/index.js
@@ -1,17 +1,13 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import { FirebaseAppProvider } from "reactfire";
-import firebaseConfig from "./firebaseConfig";
 import "./index.css";
 import App from "./App";
 import store from "./app/store";
 import { Provider } from "react-redux";
 
 ReactDOM.render(
-  <FirebaseAppProvider firebaseConfig={firebaseConfig}>
-    <Provider store={store}>
-      <App />
-    </Provider>
-  </FirebaseAppProvider>,
+  <Provider store={store}>
+    <App />
+  </Provider>,
   document.getElementById("root")
 );
