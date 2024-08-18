dcfda364fd08be320969f8becc15429aa3c55616
Alex Erdei <mralexerdei@yahoo.co.uk>
2024-08-18 18:04:52 +0100

    Fix friendListPage for vertical mobiles

--- src/components/FriendsListPage.jsx
+++ src/components/FriendsListPage.jsx
@@ -8,51 +8,57 @@ import imgFriends from "../images/friends.jpg";
 import { handleClickLink } from "./helper";
 
 const FriendsListPage = (props) => {
-  const { linkRef, activeLink, setActiveLink } = props;
+	const { linkRef, activeLink, setActiveLink } = props;
 
-  const FRIENDS_LIST_PAGE_PATH = "/fakebook/friends/list";
+	const FRIENDS_LIST_PAGE_PATH = "/fakebook/friends/list";
 
-  const location = useLocation();
+	const location = useLocation();
 
-  const isNoUser = FRIENDS_LIST_PAGE_PATH === location.pathname;
+	const isNoUser = FRIENDS_LIST_PAGE_PATH === location.pathname;
 
-  //we set the active link to the friends link when it renders
-  useEffect(() => {
-    handleClickLink(
-      { currentTarget: linkRef.current },
-      activeLink,
-      setActiveLink
-    );
-  }, [activeLink, linkRef, setActiveLink]);
+	//we set the active link to the friends link when it renders
+	useEffect(() => {
+		handleClickLink(
+			{ currentTarget: linkRef.current },
+			activeLink,
+			setActiveLink
+		);
+	}, [activeLink, linkRef, setActiveLink]);
 
-  return (
-    <Row className="overflow-hidden friends-list">
-      <FriendList />
-      <Col className="overflow-auto mh-100 hide-scrollbar col-two">
-        {isNoUser ? (
-          <div className="h-100 w-100 d-flex flex-column align-items-center justify-content-center">
-            <img
-              width="200px"
-              src={imgFriends}
-              alt="cartoon of fakebook friends"
-              className="p-4"
-            />
-            <h5 className="text-muted">
-              <b>Select people's names to preview their profile.</b>
-            </h5>
-          </div>
-        ) : (
-          <div className="profile-container">
-            <Profile
-              linkRef={linkRef}
-              activeMainLink={activeLink}
-              setActiveMainLink={setActiveLink}
-            />
-          </div>
-        )}
-      </Col>
-    </Row>
-  );
+	return window.innerWidth > 600 || isNoUser ? (
+		<Row className="overflow-hidden friends-list">
+			<FriendList />
+			<Col className="overflow-auto mh-100 hide-scrollbar col-two">
+				{isNoUser ? (
+					<div className="h-100 w-100 d-flex flex-column align-items-center justify-content-center">
+						<img
+							width="200px"
+							src={imgFriends}
+							alt="cartoon of fakebook friends"
+							className="p-4"
+						/>
+						<h5 className="text-muted">
+							<b>Select people's names to preview their profile.</b>
+						</h5>
+					</div>
+				) : (
+					<div className="profile-container">
+						<Profile
+							linkRef={linkRef}
+							activeMainLink={activeLink}
+							setActiveMainLink={setActiveLink}
+						/>
+					</div>
+				)}
+			</Col>
+		</Row>
+	) : (
+		<Profile
+			linkRef={linkRef}
+			activeMainLink={activeLink}
+			setActiveMainLink={setActiveLink}
+		/>
+	);
 };
 
 export default FriendsListPage;
